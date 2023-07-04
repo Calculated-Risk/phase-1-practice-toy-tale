4bb270299c8678379078ab727ed9b0a859ef9cd3
Edwin Soto <125782999+Calculated-Risk@users.noreply.github.com>
2023-07-03 20:56:20 -0400

    Starting lab

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
+    console.log(addToy) // ADDING CONSOLE LOG
     if (addToy) {
       toyFormContainer.style.display = "block";
     } else {
@@ -13,3 +14,26 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+/* On the index.html page, there is a div with the id "toy-collection."
+
+When the page loads, make a 'GET' request to fetch all the toy objects. With the response data, 
+make a <div class="card"> for each toy and add it to the toy-collection div.*/
+
+function fetchAllToys(){
+  fetch ("http://localhost:3000/toys")
+  .then ((response) => response.json())
+  .then ((toyList) => {
+    console.log(toyList) //console.logging to check List
+    addListToCardClass(toyList)})
+}
+fetchAllToys()
+
+function addListToCardClass(listOfToys) {
+  for (let toy of listOfToys){
+    const toyContainer = document.getElementById("toy-collection")
+    const cardClassDiv = document.createElement("div")
+    cardClassDiv.className = 'card'
+
+  }
+}
