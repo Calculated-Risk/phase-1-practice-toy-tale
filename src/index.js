d065732e3431c0e82357ba135bd3b509f0262bb1
Edwin Soto <125782999+Calculated-Risk@users.noreply.github.com>
2023-07-03 22:00:39 -0400

    added elements

--- src/index.js
+++ src/index.js
@@ -20,20 +20,41 @@ document.addEventListener("DOMContentLoaded", () => {
 When the page loads, make a 'GET' request to fetch all the toy objects. With the response data, 
 make a <div class="card"> for each toy and add it to the toy-collection div.*/
 
+
+ // Global Variables
+    const toyContainer = document.getElementById("toy-collection")
+    const cardClassDiv = document.createElement("div")
+    const H2TagName = document.createElement("h2")
+    const imgElement = document.createElement("img")
+    const toyLikesP = document.createElement("p")
+    const toyButton = document.createElement("button")
+    let toyArray = []
+    
+
 function fetchAllToys(){
   fetch ("http://localhost:3000/toys")
   .then ((response) => response.json())
   .then ((toyList) => {
     console.log(toyList) //console.logging to check List
-    addListToCardClass(toyList)})
+    addToysToCardClass(toyList)
+    toyArray = toyList
+    console.log(toyArray) //console.logging to check array
+  })
 }
 fetchAllToys()
 
-function addListToCardClass(listOfToys) {
+function addToysToCardClass(listOfToys) {
   for (let toy of listOfToys){
-    const toyContainer = document.getElementById("toy-collection")
-    const cardClassDiv = document.createElement("div")
-    cardClassDiv.className = 'card'
+    console.log(toy)
+    toyContainer.appendChild(cardClassDiv)
+    cardClassDiv.className = 'card'     
+    H2TagName.innerText = toy.name    // H2 TagName
+    cardClassDiv.append(H2TagName)
+    imgElement.src = toy.image       // Toy Image Link (displays image)
+    cardClassDiv.append(imgElement)
+    toyLikesP.innerText = `${toy.likes} likes`
+    cardClassDiv.append(toyLikesP)
+    
 
   }
 }
